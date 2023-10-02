e0c651cfc41591c70ee33500baf93b9d08abe0bf
Denis Semenov <denissemenov@Deniss-Air.attlocal.net>
2023-10-01 23:27:00 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { data } from "./data";
+import "./App.css";
 
 function App() {
+  const [hotels, setHotels] = useState(data);
+  const [showText, setShowText] = useState(false);
+
+  const removeHotel = (id) => {
+    let newHotels = hotels.filter((item) => item.id !== id);
+    setHotels(newHotels);
+  };
+
+  const showTextClick = (item) => {
+    item.showMore = !item.showMore;
+    setShowText(!showText);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <h1>NYC TOP {hotels.length} HOTELS</h1>
+      </div>
+      {hotels.map((item) => {
+        const { id, hotelName, description, image, source, showMore } = item;
+        return (
+          <div key={id}>
+            <div className="container">
+              <h2>
+                {id} - {hotelName}
+              </h2>
+            </div>
+
+            <div className="container">
+              <p>
+                {showMore ? description : description.substring(0, 220) + "..."}
+                <button onClick={() => showTextClick(item)}>
+                  {showMore ? "Show less" : "Show more"}
+                </button>
+              </p>
+            </div>
+
+            <div className="container">
+              <img src={image} width="500px" />
+            </div>
+            <div className="container">
+              <p>{source}</p>
+            </div>
+
+            <div className="container">
+              <button className="btn" onClick={() => removeHotel(id)}>
+                Remove
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
